e3f79e21d415387c177dd4798ad3fdfc83770632
CoolnsX <76195824+CoolnsX@users.noreply.github.com>
2021-07-08 20:08:38 +0530

    Add files via upload

--- bspwm/launch.sh
+++ bspwm/launch.sh
@@ -3,10 +3,15 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+# custom script
+rm $HOME/wallpapers/x.png 
+conky -c "/home/tanveer/.conky/Rock and Roll/Rock/Hermoso_Rc" & 
+conky -c "/home/tanveer/.conky/Rock and Roll/Rock 1/.conkyrc" &
+sleep 2
+maim --hidecursor -d 1 $HOME/wallpapers/x.png #screenshot of wallpaper with conky running
+killall -q conky &
+xwallpaper --stretch $HOME/wallpapers/x.png & #apply that screenshot as wallpaper
 
-# Launch
+# Launch polybar
 polybar example &
 
-echo "Bar launched..."
--- bspwm/pi.sh
+++ bspwm/pi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if ps aux | grep "picom$" ; then
 	killall -q picom
-	sleep 2
+	sleep 1
 	notify-send "picom vsync on" 
 	picom --vsync &
 else
 	killall -q picom
-	sleep 2
+	sleep 1
 	picom &
 	notify-send "picom vsync off"
 fi

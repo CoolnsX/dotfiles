8ed86c9e92d98f79e9027585980a475e0455ff28
CoolnsX <76195824+CoolnsX@users.noreply.github.com>
2021-08-16 17:38:13 +0530

    Add files via upload

--- bspwm/launch.sh
+++ bspwm/launch.sh
@@ -3,14 +3,7 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# custom script
-rm $HOME/wallpapers/x.png 
-conky -c "/home/tanveer/.conky/Rock and Roll/Rock/Hermoso_Rc" & 
-conky -c "/home/tanveer/.conky/Rock and Roll/Rock 1/.conkyrc" &
-sleep 2
-maim --hidecursor -d 1 $HOME/wallpapers/x.png #screenshot of wallpaper with conky running
-killall -q conky &
-xwallpaper --stretch $HOME/wallpapers/x.png & #apply that screenshot as wallpaper
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 polybar example &
